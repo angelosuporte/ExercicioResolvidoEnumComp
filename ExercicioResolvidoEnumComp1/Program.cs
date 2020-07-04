9bbb616a461376b6f55306f99cbc698ad8beefab
Angelo Carvalho <angelosuporteti@gmail.com>
2020-07-04 18:32:00 -0300

    Construido interação com usuário - Até instanciação do ContratoHora

--- ExercicioResolvidoEnumComp1/Program.cs
+++ ExercicioResolvidoEnumComp1/Program.cs
@@ -1,4 +1,8 @@
 ﻿using System;
+using System.Globalization;
+using ExercicioResolvidoEnumComp1.Entidades;
+using ExercicioResolvidoEnumComp1.Entidades.Enums;
+
 
 namespace ExercicioResolvidoEnumComp1
 {
@@ -6,7 +10,37 @@ namespace ExercicioResolvidoEnumComp1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.Write("Informe o departamento: ");
+            string deptoNome = Console.ReadLine();
+            Console.WriteLine("Informe os dados do colaborador: ");
+            Console.Write("Nome: ");
+            string nome = Console.ReadLine();
+            Console.Write("Nível (Junior, NívelMedio, Senior): ");
+            NivelDoColaborador nivel = Enum.Parse<NivelDoColaborador>(Console.ReadLine());
+            Console.Write("Salário Base: ");
+            double salarioBase = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Departamento depto = new Departamento(deptoNome);
+            Colaborador colaborador = new Colaborador(nome, nivel, salarioBase, depto);
+
+            Console.Write("Informe quantos são os contratos para este colaborador: ");
+            int quantContratos = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i <= quantContratos; i++)
+            {
+                Console.WriteLine($"Informe #{i} dados do contrato: ");
+                Console.Write("Data (DD/MM/YYYY): ");
+                DateTime data = DateTime.Parse(Console.ReadLine());
+                Console.Write("Valor por hora: ");
+                double valorPorHora = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                Console.Write("Informe a quantidade de horas: ");
+                int horas = int.Parse(Console.ReadLine());
+
+                ContratoHora contrato = new ContratoHora(data, valorPorHora, horas);
+
+            }
+
         }
+
     }
 }
